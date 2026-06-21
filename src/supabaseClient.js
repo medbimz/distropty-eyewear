0a2ca73c5a4e6f92929a72a86cfbbb7120b57e90
medbimz <144958270+medbimz@users.noreply.github.com>
2026-06-21 11:07:22 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://ookkzvdqugtiwihnlgtl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFmZSSIsInJlZiI6Im9va2t6dmRxdWd0aXdpaG5sZ3RsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MzQ4MzAsImV4cCI6MjA5ODExMDgzMH0.3SnhxPU7XQVmD3yVUDN1-t8GYrd5sq4bCVg59yCqvgc";
+const SUPABASE_ANON_KEY = "sb_publishable_ca5fGoPtJiYbgb0PvTki_g_giSylU7N";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
